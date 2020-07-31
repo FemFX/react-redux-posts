b9fc4fcde00daed538727f2608e15abf5452c6f1
FemFX <ignedko34@gmail.com>
2020-07-31 16:07:48 +0300

    Create actions.js

---
+++ src/redux/actions.js
@@ -0,0 +1,18 @@
+import { CREATE_POST, FETCH_POSTS } from './types';
+
+export function createPost(post) {
+  return {
+    type: CREATE_POST,
+    payload: post,
+  };
+}
+export function fetchPosts() {
+  return async (dispatch) => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=8');
+    const json = await response.json();
+    dispatch({
+      type: FETCH_POSTS,
+      payload: json,
+    });
+  };
+}
